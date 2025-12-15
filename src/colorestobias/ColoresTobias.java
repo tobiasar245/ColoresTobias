c804f98a83d8e3af48fe7da5262996ab5fb83073
Bruno Menna <Bruno@ejemplo.com>
2025-12-15 20:36:04 -0300

    Cambio de colores favoritos

--- src/colorestobias/ColoresTobias.java
+++ src/colorestobias/ColoresTobias.java
@@ -15,9 +15,9 @@ public class ColoresTobias {
      */
     public static void main(String[] args) {
         String color1, color2, color3;
-        color1 = "AZUL";
+        color1 = "NARANJA";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "AMARILLO";
         System.out.printf("%s es mi primer color favorito\n, color1");
         System.out.printf("%s es mi segundo color favorito\n, color2");
         System.out.printf("%s es mi tercer color favorito\n, color3");
