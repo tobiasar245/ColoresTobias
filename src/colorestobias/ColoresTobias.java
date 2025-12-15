938baa625368f0bee8421d1bb9e809c438dcba98
neyen <pepepeemail@sandwich.com>
2025-12-15 20:45:13 -0300

    Se amplía la lista cuatro colores

--- src/colorestobias/ColoresTobias.java
+++ src/colorestobias/ColoresTobias.java
@@ -14,13 +14,15 @@ public class ColoresTobias {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String color1, color2, color3;
+        String color1, color2, color3, color4;
         color1 = "AZUL";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "PURPURA";
+        color4 = "MAGENTA";
         System.out.printf("%s es mi primer color favorito\n, color1");
         System.out.printf("%s es mi segundo color favorito\n, color2");
         System.out.printf("%s es mi tercer color favorito\n, color3");
+        System.out.printf("%s es el ultimo color de la lista\n, color4");
     }
     
 }
