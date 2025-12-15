71cb6d8a0ccefe1e5ec9500bafa124ffa1927e71
neyen <pepepeemail@sandwich.com>
2025-12-15 20:23:19 -0300

    Primera version

---
+++ src/colorestobias/ColoresTobias.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package colorestobias;
+
+/**
+ *
+ * @author b6
+ */
+public class ColoresTobias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String color1, color2, color3;
+        color1 = "AZUL";
+        color2 = "ROJO";
+        color3 = "VERDE";
+        System.out.printf("%s es mi primer color favorito\n, color1");
+        System.out.printf("%s es mi segundo color favorito\n, color2");
+        System.out.printf("%s es mi tercer color favorito\n, color3");
+    }
+    
+}
