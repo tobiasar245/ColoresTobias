5b5bcb7cab5d65cef8af8e0767bead4e7ebdf65d
Bruno Menna <Bruno@ejemplo.com>
2025-12-15 21:03:09 -0300

    Errores de los print corregidos

--- src/colorestobias/ColoresTobias.java
+++ src/colorestobias/ColoresTobias.java
@@ -12,10 +12,10 @@ public class ColoresTobias {
         color2 = "ROJO";
         color3 = "PURPURA";
         color4 = "MAGENTA";
-        System.out.printf("%s es mi primer color favorito\n, color1");
-        System.out.printf("%s es mi segundo color favorito\n, color2");
-        System.out.printf("%s es mi tercer color favorito\n, color3");
-        System.out.printf("%s es el ultimo color de la lista\n, color4");
+        System.out.printf("%s es mi primer color favorito\n", color1);
+        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s es mi tercer color favorito\n", color3);
+        System.out.printf("%s es el ultimo color de la lista\n", color4);
     }
     
 }
